40f4c3a648d9e08af003841f97dfb4ab0a119630
Peter Koval <koval.peter@gmail.com>
2025-09-06 10:00:12 +0200

    try after reformating

--- test/tracker/track/test_second_det.py
+++ test/tracker/track/test_second_det.py
@@ -13,8 +13,7 @@ def test_second_det(tracker: Ab3DMot, det_reports1: Dict) -> None:
     tracker.track(det_reports1)
     results = tracker.track(det_reports1)
     assert len(results) == 1
-    ref0 = [[8., 2., 3., 4., 5., 6., 0.7168146928204138, 1., 1.1, 2.1, 3.1, 4.1, 5.1]]
+    ref0 = [[8.0, 2.0, 3.0, 4.0, 5.0, 6.0, 0.7168146928204138, 1.0, 1.1, 2.1, 3.1, 4.1, 5.1]]
     assert results[0] == pytest.approx(np.array(ref0))
     assert len(tracker.trackers) == 1
-    assert tracker.id_now_output == pytest.approx([1.])
-
+    assert tracker.id_now_output == pytest.approx([1.0])
